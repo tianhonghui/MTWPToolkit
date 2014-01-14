adcf5fdc5f3c574630cb5c5b3a53c54c49d38d44
tianhonghui <tianhonghui@meituan.com>
2014-01-14 11:32:23 +0800

    添加bulletControl&MTImage&MTFadeIn

---
+++ MTWPToolkit/MTWPToolkit/Controls/MTBulletControl.cs
@@ -0,0 +1,95 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace MTWPToolkit.Controls {
+    public class MTBulletControl : Control {
+        public static readonly DependencyProperty ImageSourceProperty =
+            DependencyProperty.Register("ImageSource", typeof(ImageSource), typeof(MTBulletControl),
+                new PropertyMetadata(default(ImageSource)));
+
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(MTBulletControl),
+                new PropertyMetadata("bulletControl"));
+
+        public static readonly DependencyProperty ImageWidthProperty =
+            DependencyProperty.Register("ImageWidth", typeof(double), typeof(MTBulletControl),
+                new PropertyMetadata(24.0));
+
+        public static readonly DependencyProperty ImageHeightProperty =
+            DependencyProperty.Register("ImageHeight", typeof(double), typeof(MTBulletControl),
+                new PropertyMetadata(24.0));
+
+        public static readonly DependencyProperty TextMarginProperty =
+            DependencyProperty.Register("TextMargin", typeof(Thickness), typeof(MTBulletControl),
+                new PropertyMetadata(default(Thickness)));
+
+        public static readonly DependencyProperty TextForegroundProperty =
+            DependencyProperty.Register("TextForeground", typeof(Brush), typeof(MTBulletControl),
+                new PropertyMetadata(Application.Current.Resources["PhoneForegroundBrush"], TextForegroundChanged));
+
+        public static readonly DependencyProperty TextFontSizeProperty =
+            DependencyProperty.Register("TextFontSize", typeof(double), typeof(MTBulletControl),
+                new PropertyMetadata(24.0, TextFontSizeChanged));
+
+        public ImageSource ImageSource {
+            get { return (ImageSource)GetValue(ImageSourceProperty); }
+            set { SetValue(ImageSourceProperty, value); }
+        }
+
+        public string Text {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public double ImageWidth {
+            get { return (double)GetValue(ImageWidthProperty); }
+            set { SetValue(ImageWidthProperty, value); }
+        }
+
+        public double ImageHeight {
+            get { return (double)GetValue(ImageHeightProperty); }
+            set { SetValue(ImageHeightProperty, value); }
+        }
+
+        public Thickness TextMargin {
+            get { return (Thickness)GetValue(TextMarginProperty); }
+            set { SetValue(TextMarginProperty, value); }
+        }
+
+        public Brush TextForeground {
+            get { return (Brush)GetValue(TextForegroundProperty); }
+            set { SetValue(TextForegroundProperty, value); }
+        }
+
+        public double TextFontSize {
+            get { return (double)GetValue(TextFontSizeProperty); }
+            set { SetValue(TextFontSizeProperty, value); }
+        }
+
+        public const string ContentTextBlockKey = "ContentTextBlock";
+        public TextBox ContentTextBlock { get; set; }
+        public override void OnApplyTemplate() {
+            base.OnApplyTemplate();
+            ContentTextBlock = GetTemplateChild("ContentTextBlock") as TextBox;
+
+        }
+
+        public MTBulletControl() {
+            DefaultStyleKey = typeof(MTBulletControl);
+        }
+        private static void TextForegroundChanged(DependencyObject dependencyObject,
+            DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs) {
+            var control = dependencyObject as MTBulletControl;
+            if (control != null)
+                control.ContentTextBlock.Foreground = dependencyPropertyChangedEventArgs.NewValue as Brush;
+        }
+
+        private static void TextFontSizeChanged(DependencyObject dependencyObject,
+            DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs) {
+            var control = dependencyObject as MTBulletControl;
+            if (control != null)
+                control.ContentTextBlock.FontSize = (double)dependencyPropertyChangedEventArgs.NewValue;
+        }
+    }
+}
---
+++ MTWPToolkit/MTWPToolkit/UIExtenstion/MTFadeIn.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace MTWPToolkit.UIExtenstion {
+    /// <summary>
+    /// 用途：为控件添加FadeIn显示效果
+    /// FadeLevel 表示FadeIn的级别，级别越高FadeIn的动画执行时间越长
+    /// </summary>
+    public class MTFadeIn {
+
+        private const int MilliSecondsPerLevel = 350;
+
+        public static readonly DependencyProperty FadeLevelProperty =
+            DependencyProperty.RegisterAttached("FadeLevel",typeof(double),typeof(MTFadeIn),new PropertyMetadata(default(double),PropertyChangedCallback));
+
+        private static void PropertyChangedCallback(DependencyObject e,DependencyPropertyChangedEventArgs args) {
+            var element = e as FrameworkElement;
+            if(element != null) {
+                element.Opacity = 0;
+                element.Loaded+=ElementOnLoaded;
+            }
+        }
+
+
+        public static void SetFadeLevel(FrameworkElement element,double value) {
+            element.SetValue(FadeLevelProperty,value);
+        }
+
+        public static double GetFadeLevel(FrameworkElement element) {
+            return (double)element.GetValue(FadeLevelProperty);
+        }
+
+        private static async void ElementOnLoaded(object sender,RoutedEventArgs routedEventArgs) {
+            var element = sender as FrameworkElement;
+            if(element != null) {
+                element.Loaded -= ElementOnLoaded;
+                    var level = GetFadeLevel(element)*MilliSecondsPerLevel;
+                    var sb = new Storyboard();
+                    var anim = new DoubleAnimation {
+                        From = 0,
+                        To = 1,
+                        Duration = TimeSpan.FromMilliseconds(level),
+                        EasingFunction = new CircleEase() { EasingMode = EasingMode.EaseIn }
+                    };
+                    Storyboard.SetTarget(anim,element);
+                    Storyboard.SetTargetProperty(anim,new PropertyPath("Opacity"));
+                    sb.Children.Add(anim);
+                    sb.Begin();
+            }
+        }
+
+    }
+}
